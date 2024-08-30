4bcb08943511324ce2da0a5e575dd19122836d43
sultanovttIOS <alisher192377@gmail.com>
2024-08-30 14:45:23 +0600

    Initial Commit

---
+++ GeeksStudio/GeeksStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeeksStudioApp.swift
+//  GeeksStudio
+//
+//  Created by Alisher Sultanov on 30/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GeeksStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
